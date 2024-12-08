7bcaab9206e96651d34a63efbcba3c18a2dc7ab3
nicokant <niccolocantu@gmail.com>
2024-12-08 14:28:36 +0100

    add part 2

--- 2/script.jl
+++ 2/script.jl
@@ -11,12 +11,32 @@ file = "2/input.txt"
 data = CSV.read(file, DataFrame; header=false, delim=' ')
 
 
+function is_safe(vec::Vector{Int64})
+    diffs = [e - vec[index + 1] for (index, e) in enumerate(vec[1:end-1])]
+    return !(minimum(diffs) < 0 < maximum(diffs)) && all((n  ->  0 < abs(n) < 4).(diffs))
+end
+
 # Part 1
 safe = Vector()
 for row in eachrow(data)
     cleaned_row = collect(skipmissing(row))
-    diffs = [e - cleaned_row[index + 1] for (index, e) in enumerate(cleaned_row[1:end-1])]
-    if !(minimum(diffs) < 0 < maximum(diffs)) && all((n  ->  0 < abs(n) < 4).(diffs))
+    if is_safe(cleaned_row)
+        push!(safe, cleaned_row)
+    end
+end
+length(safe)
+
+
+# Part 2
+# Tolerate one error
+
+safe = Vector()
+for row in eachrow(data)
+    cleaned_row = collect(skipmissing(row))
+    if any([
+        is_safe(
+            vcat(cleaned_row[1:i-1], cleaned_row[i+1:end])
+        ) for (i, e) in enumerate(cleaned_row)])
         push!(safe, cleaned_row)
     end
 end
