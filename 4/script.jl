c0f2f6f2aec3d32464a8e1f556ed06864a0a2c72
nicokant <niccolocantu@gmail.com>
2024-12-09 21:17:53 +0100

    negative index diagonals

--- 4/script.jl
+++ 4/script.jl
@@ -1,26 +1,30 @@
 import Pkg;
 Pkg.add("CSV");
-Pkg.add("DataFrames");
 
-using CSV, DataFrames, LinearAlgebra
+using CSV, LinearAlgebra
 
 
 file = "4/input.txt"
 
 function xmas_occurrences(str::String)
     re = r"(XMAS|SAMX)"
-    sum([length(collect(eachmatch(re, s))) for s in str])
+    length(collect(eachmatch(re, str)))
 end
 
-open(file) do f
-    rows = [split(l, "") for l in readlines(f)]
-    matrix = mapreduce(permutedims, vcat, rows)
+f = open(file)
+rows = [split(l, "") for l in readlines(f)]
+matrix = mapreduce(permutedims, vcat, rows)
 
-    rows, cols = size(matrix)
+reversed_matrix = reverse(matrix)
+r_count, c_count= size(matrix)
 
-    xmas_occurrences(join.(eachrow(matrix))) + xmas_occurrences(join.(eachcol(matrix))) +
-    xmas_occurrences([join(diag(matrix, i)) for i in range(1, rows + 1)]) +
-    xmas_occurrences([join(diag(reverse(matrix), i)) for i in range(1, rows + 1)])
-end
+sum([
+    xmas_occurrences.(join.(eachrow(matrix)));
+    xmas_occurrences.(join.(eachcol(matrix)));
+    xmas_occurrences.([join(diag(matrix, i)) for i in range(-r_count, r_count)]);
+    xmas_occurrences.([join(diag(reversed_matrix, i)) for i in range(-r_count, r_count)])
+])
 
+close(f)
 
+# NOTE: this is still wrong
