adaa3cb012a38bb8287d417ddcfacce56f59e9c0
Niccolò Cantù <niccolo.cantu@nina.no>
2024-12-13 11:32:48 +0100

    change ex3

--- 3/script.jl
+++ 3/script.jl
@@ -3,8 +3,49 @@
 
 re = r"mul\((\d+),(\d+)\)"
 
-open("3/input.txt", "r") do file
-    println(sum([sum([prod(parse.(Int64,match.captures)) for match in eachmatch(re, line)]) for line in readlines(file)]))
+function count_mul(s::String)
+    sum([prod(parse.(Int64,match.captures)) for match in eachmatch(re, s)])
 end
+
+open("3/input.txt", "r") do file
+    println(sum([count_mul(line) for line in readlines(file)]))
+end
+
+
+re2 = r"(^|do\(\))(.*?)(don't\(\)|$)"
+file = open("3/input.txt", "r")
+
+lines = readlines(file)
+non_corrupted = [match for line in lines for match in eachmatch(re2, "do()" * line * "don't()")]
+
+count_mul(non_corrupted)
+
+# println(lines[1])
+
+# parts = []
+
+# for l in lines
+#     do_range = UnitRange{Int64}(1,1)
+#     while !isnothing(do_range)
+#         dont_range = findnext("don't()", l, do_range.start)
+#         if isnothing(dont_range)
+#             push!(parts, count_mul(l[do_range.stop:end]))
+#             break
+#         end
+#         push!(parts, count_mul(l[do_range.stop:dont_range.start]))
+#         do_range = findnext(r"do()", l, dont_range.stop)
+#     end
+# end
+
+# sum(parts)
+
+# for s in split(lines[1], "do()")
+#     for p in split(s, "don't()")
+#         println(p, "don't()")
+#     end
+#     println("do()")
+# end
+
+# sum(count_mul.([join(split(s, "don't()")[1:end-1]) for line in lines for s in split(line, "do()") ]))
 
 
